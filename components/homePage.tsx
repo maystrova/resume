d764b402614151b43c4cbfdbdfdf713f6ed156c0
maystrova <maystrovakate@gmail.com>
2022-08-30 18:01:09 +0300

    homePage photo

--- components/homePage.tsx
+++ components/homePage.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
 import styles from 'styles/homePage.module.scss'
 import Header from 'components/header'
+import { getStorage, ref, getDownloadURL } from 'firebase/storage'
 
 interface HomePageProps {}
 
 const HomePage = ({}: HomePageProps) => {
-    const [profilePhoto, setProfilePhoto] = useState<string>('')
+    const [profilePhoto, setProfilePhoto] = useState<string>('/static/homePagePhoto2.PNG')
 
     return (
         <div className={styles.container}>
             <Header />
-            <img src={profilePhoto} alt="" />
+            <img className={styles.homePagePhoto} src={profilePhoto} alt="" />
         </div>
     )
 }

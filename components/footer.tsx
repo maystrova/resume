f2d71b758e79eed1db10de272b0b1eb86b5da02a
maystrova <maystrovakate@gmail.com>
2022-10-21 19:26:55 +0300

    copy button footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,15 @@
+import Icon, { ICON_SIZE } from 'components/icon'
 import styles from 'styles/footer.module.scss'
-import Icon, { ICON_SIZE } from './icon'
+import { useState } from 'react'
+import Button, { BUTTON_TYPE } from './button'
 
 interface FooterProps {}
 
 const Footer = ({}: FooterProps) => {
+    const [copySuccess, setCopySuccess] = useState<boolean>(false)
+
     const name: string = 'Kseniia Popova'
+    const email: string = 'ksenia.popova93@gmail.com'
     return (
         <div className={styles.container}>
             <div className={styles.main}>
@@ -13,7 +18,31 @@ const Footer = ({}: FooterProps) => {
                 </div>
                 <div className={styles.contacts}>
                     <Icon src={'static/mail-icon.svg'} alt={'icon'} size={ICON_SIZE.X_SMALL} />
-                    <span>ksenia.popova93@gmail.com</span>
+                    <span className={styles.email}>{email}</span>
+                    <div className={styles.copy}>
+                        <Button
+                            onClick={() => {
+                                navigator.clipboard
+                                    .writeText(email)
+                                    .then(() => {
+                                        setCopySuccess(true)
+                                    })
+                                    .catch(error => {
+                                        console.log(error)
+                                    })
+                            }}
+                            btnType={BUTTON_TYPE.CLOSE_WINDOW}
+                        >
+                            <Icon
+                                src={
+                                    copySuccess ? 'static/success-icon.svg' : 'static/copy-icon.svg'
+                                }
+                                alt={'Copy'}
+                                size={ICON_SIZE.SMALL}
+                            />
+                        </Button>
+                        {copySuccess && <span>Copied!</span>}
+                    </div>
                 </div>
             </div>
         </div>

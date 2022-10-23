c4d56aff6e7ab77cb5776276cddae0fa8c42fcf3
maystrova <maystrovakate@gmail.com>
2022-10-23 18:03:07 +0300

    footer-features

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,6 @@
 import Icon, { ICON_SIZE } from 'components/icon'
 import styles from 'styles/footer.module.scss'
 import { useState } from 'react'
-import Button, { BUTTON_TYPE } from './button'
 
 interface FooterProps {}
 
@@ -10,39 +9,30 @@ const Footer = ({}: FooterProps) => {
 
     const name: string = 'Kseniia Popova'
     const email: string = 'ksenia.popova93@gmail.com'
+
+    const copyEmail = () => {
+        navigator.clipboard
+            .writeText(email)
+            .then(() => {
+                setCopySuccess(true)
+                setTimeout(() => {
+                    setCopySuccess(false)
+                }, 1000)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
     return (
         <div className={styles.container}>
             <div className={styles.main}>
                 <div className={styles.logo}>
                     {new Date().getFullYear()} {name}
                 </div>
-                <div className={styles.contacts}>
+                <div className={styles.contacts} onClick={copyEmail}>
                     <Icon src={'static/mail-icon.svg'} alt={'icon'} size={ICON_SIZE.X_SMALL} />
                     <span className={styles.email}>{email}</span>
-                    <div className={styles.copy}>
-                        <Button
-                            onClick={() => {
-                                navigator.clipboard
-                                    .writeText(email)
-                                    .then(() => {
-                                        setCopySuccess(true)
-                                    })
-                                    .catch(error => {
-                                        console.log(error)
-                                    })
-                            }}
-                            btnType={BUTTON_TYPE.CLOSE_WINDOW}
-                        >
-                            <Icon
-                                src={
-                                    copySuccess ? 'static/success-icon.svg' : 'static/copy-icon.svg'
-                                }
-                                alt={'Copy'}
-                                size={ICON_SIZE.SMALL}
-                            />
-                        </Button>
-                        {copySuccess && <span>Copied!</span>}
-                    </div>
+                    <span className={copySuccess ? styles.copy_success : styles.hide}>Copied!</span>
                 </div>
             </div>
         </div>
